03ac2e1d206521b25db579e1c27113eae0472f5b
Robert Metzger <metzgerr@web.de>
2014-05-07 11:03:35 +0200

    added deflate writer

---
+++ src/main/java/eu/stratosphere/test/testPlan/DeflateWriterJob.java
@@ -0,0 +1,46 @@
+package eu.stratosphere.test.testPlan;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+
+import eu.stratosphere.api.common.JobExecutionResult;
+import eu.stratosphere.api.common.Plan;
+import eu.stratosphere.api.common.Program;
+import eu.stratosphere.api.common.accumulators.AccumulatorHelper;
+import eu.stratosphere.api.common.operators.FileDataSource;
+import eu.stratosphere.api.java.record.io.TextInputFormat;
+import eu.stratosphere.client.LocalExecutor;
+import eu.stratosphere.hadoopcompatibility.HadoopDataSink;
+
+public class DeflateWriterJob implements Program {
+
+	public Plan getPlan(String... args) {
+		String inputPath = args[0];
+		String deflateOut = args[1];
+		int dop = Integer.parseInt(args[2]);
+		
+		JobConf jobConf = new JobConf();
+
+		FileDataSource src = new FileDataSource(TextInputFormat.class, inputPath);
+		
+		HadoopDataSink<Text, IntWritable> out = new HadoopDataSink<Text, IntWritable>(new org.apache.hadoop.mapred.TextOutputFormat<Text, IntWritable>(), jobConf, 
+				"Hadoop TextOutputFormat (w/ deflate)", src, Text.class,IntWritable.class);
+		
+		org.apache.hadoop.mapred.TextOutputFormat.setOutputPath(out.getJobConf(), new Path(deflateOut));
+		
+		jobConf.setBoolean("mapred.output.compress", true);
+		
+		Plan p = new Plan(out, "Deflate creat0r");
+		p.setDefaultParallelism(dop);
+		return p;
+	}
+
+	public static void main(String[] args) throws Exception {
+		DeflateWriterJob sqT = new DeflateWriterJob();
+		JobExecutionResult res = LocalExecutor.execute(sqT.getPlan(args));
+		System.err.println("Result:\n"+AccumulatorHelper.getResultsFormated(res.getAllAccumulatorResults()));
+	}
+
+}
---
+++ src/main/java/eu/stratosphere/test/testPlan/ReadWriteTest.java
@@ -0,0 +1,93 @@
+package eu.stratosphere.test.testPlan;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+
+import eu.stratosphere.api.common.JobExecutionResult;
+import eu.stratosphere.api.common.Plan;
+import eu.stratosphere.api.common.Program;
+import eu.stratosphere.api.common.accumulators.AccumulatorHelper;
+import eu.stratosphere.api.common.accumulators.LongCounter;
+import eu.stratosphere.api.common.operators.FileDataSink;
+import eu.stratosphere.api.common.operators.FileDataSource;
+import eu.stratosphere.api.common.operators.Operator;
+import eu.stratosphere.api.java.record.functions.MapFunction;
+import eu.stratosphere.api.java.record.io.CsvOutputFormat;
+import eu.stratosphere.api.java.record.io.TextInputFormat;
+import eu.stratosphere.api.java.record.operators.CoGroupOperator;
+import eu.stratosphere.api.java.record.operators.MapOperator;
+import eu.stratosphere.client.LocalExecutor;
+import eu.stratosphere.configuration.Configuration;
+import eu.stratosphere.core.fs.FileSystem.WriteMode;
+import eu.stratosphere.types.Record;
+import eu.stratosphere.types.StringValue;
+import eu.stratosphere.util.Collector;
+
+/**
+ * Reads data from a directory and writes it again (to test reading deflate files)
+ *
+ */
+public class ReadWriteTest implements Program {
+
+	public static class SameIn extends MapFunction {
+		
+		private static int i = 0;
+		@Override
+		public void map(Record record, Collector<Record> out) throws Exception {
+		//	System.err.println("Record "+ (i++) +" : "+record.getField(0, StringValue.class));
+			
+		}
+		
+	}
+	public static class Counting extends MapFunction {
+
+		LongCounter tup;
+		@Override
+		public void open(Configuration parameters) throws Exception {
+			super.open(parameters);
+			tup = getRuntimeContext().getLongCounter("tuples");
+		}
+		@Override
+		public void map(Record record, Collector<Record> out) throws Exception {
+			// System.err.println("Line cont: "+record.getField(0, StringValue.class).getValue());
+			tup.add(1L);
+			out.collect(record);
+		}
+		
+	}
+	public Plan getPlan(String... args) {
+		String in = args[0];
+		String correct = args[1];
+		String out = args[2];
+		int dop = Integer.parseInt(args[3]);
+		
+		FileDataSource src = new FileDataSource(new TextInputFormat(), in);
+		//FileDataSource correctSrc = new FileDataSource(new TextInputFormat(), correct);
+		List<Operator> inL = new ArrayList(2);
+		inL.add(src); //inL.add(correctSrc);
+	//	MapOperator sameIn = MapOperator.builder(new SameIn() ).inputs( inL ).build();
+		MapOperator cnt = MapOperator.builder(new Counting()).input(src).build();
+		CsvOutputFormat cof = new CsvOutputFormat(StringValue.class);
+		cof.setWriteMode(WriteMode.OVERWRITE);
+		FileDataSink sink = new FileDataSink(cof, out);
+		sink.setInput(cnt);
+		
+	//	FileDataSink fakeSink = new FileDataSink(cof, "file:///dev/null");
+	//	fakeSink.setInput(sameIn);
+		Plan p = new Plan((Collection)ImmutableSet.of(sink /*, fakeSink*/ ), "ReadWrite Test");
+		p.setDefaultParallelism(dop);
+		return p;
+	}
+
+	public static void main(String[] args) throws Exception {
+		ReadWriteTest sqT = new ReadWriteTest();
+		JobExecutionResult res = LocalExecutor.execute(sqT.getPlan(args));
+		System.err.println("Result:\n"+AccumulatorHelper.getResultsFormated(res.getAllAccumulatorResults()));
+	}
+
+}
