e88ce091f254c76193c5b4d8c306418fa6b90df8
uce <u.celebi@fu-berlin.de>
2014-07-29 14:38:35 +0200

    Fix Java API WordCount without combine run script

--- automation/runWC-JAPI-withoutCombine.sh
+++ automation/runWC-JAPI-withoutCombine.sh
@@ -6,5 +6,4 @@ echo "Running wordcount japi without combine example"
 
 ARGS="$HDFS_WC $HDFS_WC_OUT"
 echo "running wc with args $ARGS"
-$FLINK_BUILD_HOME"/bin/flink" run -p $DOP -c org.apache.flink.test.testPlan.WordCountWithoutCombine -j $TESTJOB_HOME"/target/testjob-*.jar" $ARGS
-
+$FLINK_BUILD_HOME"/bin/flink" run -p $DOP -c com.github.projectflink.testPlan.WordCountWithoutCombine -j $TESTJOB_HOME"/target/flink-perf-*.jar" $ARGS
