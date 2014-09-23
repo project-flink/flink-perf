63f5711b4c1426c24c68d6c35b728a283509d3e0
mingliang <qmlmoon@gmail.com>
2014-09-23 21:53:30 +0800

    Fix package breaks

--- flink-jobs/src/main/java/com/github/projectflink/testPlan/KMeansArbitraryDimension.java
+++ flink-jobs/src/main/java/com/github/projectflink/testPlan/KMeansArbitraryDimension.java
@@ -22,13 +22,13 @@ import java.util.Collection;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.functions.RichMapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.IterativeDataSet;
+import org.apache.flink.api.java.operators.IterativeDataSet;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.util.StringUtils;
 
