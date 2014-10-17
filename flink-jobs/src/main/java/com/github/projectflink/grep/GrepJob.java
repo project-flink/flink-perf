2af0c62bfabb3ee11e18387ea22d554e580243ba
Robert Metzger <rmetzger@apache.org>
2014-10-17 16:38:46 +0200

    extend both grep implementations to support to grep for multiple terms into different files

--- flink-jobs/src/main/java/com/github/projectflink/grep/GrepJob.java
+++ flink-jobs/src/main/java/com/github/projectflink/grep/GrepJob.java
@@ -9,48 +9,64 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+/**
+ * This job allows to grep text file for different terms.
+ *
+ */
 public class GrepJob {
 
 	public static void main(final String[] args) throws Exception {
 		// set up the execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-		System.err.println("Using input="+args[0]);
-		System.err.println("Using output="+args[1]);
-		System.err.println("Using pattern'"+args[2]+"'");
+		String in = args[0];
+		String out = args[1];
+		System.err.println("Using input="+in);
+		System.err.println("Using output="+out);
+
+		String patterns[] = new String[args.length -2];
+		System.arraycopy(args,2,patterns,0,args.length-2);
+		System.err.println("Using patterns: "+ Arrays.toString(patterns));
+
 		// get input data
 		DataSet<String> text = env.readTextFile(args[0]);
-		DataSet<String> res = text.filter(new RichFilterFunction<String>() {
-			Pattern p = Pattern.compile(args[2]);
-			LongCounter filterMatches = new LongCounter();
-			LongCounter filterRecords = new LongCounter();
-			@Override
-			public void open(Configuration parameters) throws Exception {
-				super.open(parameters);
-				getRuntimeContext().addAccumulator("filterMatchCount", filterMatches);
-				getRuntimeContext().addAccumulator("filterRecordCount", filterRecords);
-			}
+		for(int p = 0;p < patterns.length; p++) {
+			final String pattern = patterns[p];
+			DataSet<String> res = text.filter(new RichFilterFunction<String>() {
+				Pattern p = Pattern.compile(pattern);
+				LongCounter filterMatches = new LongCounter();
+				LongCounter filterRecords = new LongCounter();
 
-			@Override
-			public boolean filter(String value) throws Exception {
-				filterRecords.add(1L);
-				if(value == null || value.length() == 0) {
+				@Override
+				public void open(Configuration parameters) throws Exception {
+					super.open(parameters);
+					getRuntimeContext().addAccumulator("filterMatchCount-"+pattern, filterMatches);
+					getRuntimeContext().addAccumulator("filterRecordCount-"+pattern, filterRecords);
+				}
+
+				@Override
+				public boolean filter(String value) throws Exception {
+					filterRecords.add(1L);
+					if (value == null || value.length() == 0) {
+						return false;
+					}
+					final Matcher m = p.matcher(value);
+					if (m.find()) {
+						filterMatches.add(1L);
+						return true;
+					}
 					return false;
 				}
-				final Matcher m = p.matcher(value);
-				if(m.find() ) {
-					filterMatches.add(1L);
-					return true;
-				}
-				return false;
-			}
-		});
-		res.writeAsText(args[1], FileSystem.WriteMode.OVERWRITE);
+			}).name("grep for "+pattern);
+			res.writeAsText(out+"_"+pattern, FileSystem.WriteMode.OVERWRITE);
+		}
 
 		// execute program
-		JobExecutionResult jobResult = env.execute("Flink Filter benchmark");
+		JobExecutionResult jobResult = env.execute("Flink Grep benchmark");
 		System.err.println(AccumulatorHelper.getResultsFormated(jobResult.getAllAccumulatorResults()));
 	}
 }
--- spark-jobs/src/main/java/com/github/projectflink/spark/Grep.java
+++ spark-jobs/src/main/java/com/github/projectflink/spark/Grep.java
@@ -8,6 +8,7 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,30 +19,35 @@ public class Grep {
 		String master = args[0];
 		String inFile = args[1];
 		String outFile = args[2];
-		final String regex = args[3];
+
+		String patterns[] = new String[args.length-3];
+		System.arraycopy(args,3,patterns,0,args.length-3);
 		System.err.println("Starting spark with master="+master+" in="+inFile);
-		
-		SparkConf conf = new SparkConf().setAppName("Read only job").setMaster(master).set("spark.hadoop.validateOutputSpecs", "false");
+		System.err.println("Using patterns: "+ Arrays.toString(patterns));
+
+		SparkConf conf = new SparkConf().setAppName("Grep job").setMaster(master).set("spark.hadoop.validateOutputSpecs", "false");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
 		JavaRDD<String> file = sc.textFile(inFile);
-		JavaRDD<String> res = file.filter(new Function<String, Boolean>() {
-			private static final long serialVersionUID = 1L;
-			Pattern p = Pattern.compile(regex);
+		for(int p = 0; p < patterns.length; p++) {
+			final String pattern = patterns[p];
+			JavaRDD<String> res = file.filter(new Function<String, Boolean>() {
+				private static final long serialVersionUID = 1L;
+				Pattern p = Pattern.compile(pattern);
 
-			@Override
-			public Boolean call(String value) throws Exception {
-				if(value == null || value.length() == 0) {
+				@Override
+				public Boolean call(String value) throws Exception {
+					if (value == null || value.length() == 0) {
+						return false;
+					}
+					final Matcher m = p.matcher(value);
+					if (m.find()) {
+						return true;
+					}
 					return false;
 				}
-				final Matcher m = p.matcher(value);
-				if(m.find() ) {
-					return true;
-				}
-				return false;
-			}
-		});
-		res.saveAsTextFile(outFile);
-
+			});
+			res.saveAsTextFile(outFile+"_"+pattern);
+		}
 	}
 }
