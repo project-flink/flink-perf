fec84a8cf71de8cbb6834b1a0c12f5934716f293
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2014-10-26 16:47:43 +0100

    Switch HashAgg to Mutable Objects

--- flink-jobs/src/main/scala/com/github/projectflink/GroupReduceBenchmark.scala
+++ flink-jobs/src/main/scala/com/github/projectflink/GroupReduceBenchmark.scala
@@ -22,7 +22,6 @@ import java.lang.management.ManagementFactory
 
 import org.apache.flink.api.common.accumulators.LongCounter
 import org.apache.flink.api.common.functions.RichMapPartitionFunction
-import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.common.operators.base.JoinOperatorBase.JoinHint
 import org.apache.flink.api.scala._
 import org.apache.flink.configuration.Configuration
@@ -32,11 +31,52 @@ import org.apache.flink.util.Collector
 import scala.collection.mutable
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
-case class JTuple3[T1, T2, T3](var _1: T1, var _2: T2, var _3: T3)
-case class JTuple2[T1, T2](var _1: T1, var _2: T2)
+class MTuple3[T1, T2, T3](var _1: T1, var _2: T2, var _3: T3) {
+  def this() = this(null.asInstanceOf[T1], null.asInstanceOf[T2], null.asInstanceOf[T3])
+
+  override def hashCode = 73*_1.hashCode() + 79*_2.hashCode() + _3.hashCode()
+  override def equals(other: Any): Boolean = {
+    if (!(other.isInstanceOf[MTuple3[_, _, _]])) {
+      return false
+    }
+    val oTuple = other.asInstanceOf[MTuple3[_, _, _]]
+    _1.equals(oTuple._1) && _2.equals(oTuple._2) && _3.equals(oTuple._3)
+  }
+
+  override def toString = "M(" + _1 + "," + _2 + "," + _3 + ")"
+}
+
+class MTuple2[T1, T2](var _1: T1, var _2: T2) {
+  def this() = this(null.asInstanceOf[T1], null.asInstanceOf[T2])
+
+  override def hashCode = 73*_1.hashCode() + _2.hashCode()
+  override def equals(other: Any): Boolean = {
+    if (!(other.isInstanceOf[MTuple2[_, _]])) {
+      return false
+    }
+    val oTuple = other.asInstanceOf[MTuple2[_, _]]
+    _1.equals(oTuple._1) && _2.equals(oTuple._2)
+  }
+
+  override def toString = "M(" + _1 + "," + _2 + ")"
+}
+
+class MTuple2Cheat[T1, T2](var _1: T1, var _2: T2) {
+  def this() = this(null.asInstanceOf[T1], null.asInstanceOf[T2])
+
+  override def hashCode = _1.hashCode()
+  override def equals(other: Any): Boolean = {
+    if (!(other.isInstanceOf[MTuple2Cheat[_, _]])) {
+      return false
+    }
+    val oTuple = other.asInstanceOf[MTuple2Cheat[_, _]]
+    _1.equals(oTuple._1)
+  }
+
+  override def toString = "M(" + _1 + "," + _2 + ")"
+}
 
 object GroupReduceBenchmarkGenerateData {
   val rnd = new Random(System.currentTimeMillis())
@@ -136,9 +176,9 @@ object GroupReduceBenchmarkFlink {
 
     // Group by country and determine top-k books
     val result = readsWithCountryAndBook
-      .map { in => (in._1, in._2, 1L) }
-      .groupBy("_2", "_1")
-      .sum("_3")
+      .map { in => new MTuple2(new MTuple2(in._1, in._2), 1L) }
+      .groupBy("_1.*")
+      .sum("_2")
 //      .groupBy("_1").sortGroup("_3", Order.DESCENDING)
 //      .first(k)
 //      .groupBy("_1")
@@ -187,8 +227,8 @@ object GroupReduceBenchmarkFlinkHashCombine {
 
     // Group by country and determine top-k books
     val result = readsWithCountryAndBook
-      .map { in => new JTuple2((in._1, in._2), 1L) }
-      .mapPartition(new HashListAggregator("COMBINER"))
+      .map { in => new MTuple2Cheat(new MTuple2(in._1, in._2), 1L) }
+      .mapPartition(new MutableHashAggregator("COMBINER"))
       .partitionByHash("_1.*")
       .groupBy("_1.*")
       .sum("_2")
@@ -237,13 +277,21 @@ object GroupReduceBenchmarkFlinkHash {
     env.setDegreeOfParallelism(dop)
 
     val readsWithCountryAndBook = env.readCsvFile[(String, String)](inputPath)
+//    val readsWithCountryAndBook = env.fromElements(
+//      ("Land 1", "Song 1"),
+//      ("Land 1", "Song 1"),
+//      ("Land 1", "Song 1"),
+//      ("Land 3", "Song 3"),
+//      ("Land 3", "Song 3"),
+//      ("Land 2", "Song 2"))
 
     // Group by country and determine top-k books
     val result = readsWithCountryAndBook
-      .map { in => new JTuple2((in._1, in._2), 1L) }
-      .mapPartition(new HashListAggregator("COMBINER"))
+      .map { in => new MTuple2Cheat(new MTuple2(in._1, in._2), 1L) }
+      .mapPartition(new MutableHashAggregator("COMBINER"))
       .partitionByHash("_1.*")
-      .mapPartition(new HashListAggregator("REDUCER"))
+      .mapPartition(new MutableHashAggregator("REDUCER"))
+      .map { in => (in._1._1, in._1._2, in._2) }
 //      .groupBy("_1").sortGroup("_3", Order.DESCENDING)
 //      .first(k)
 //      .groupBy("_1")
@@ -266,24 +314,30 @@ object GroupReduceBenchmarkFlinkHash {
   }
 }
 
-class HashListAggregator(combine: String)
-  extends RichMapPartitionFunction[JTuple2[(String, String), Long], JTuple2[(String, String), Long]] {
+class MutableHashAggregator(combine: String)
+  extends RichMapPartitionFunction[
+    MTuple2Cheat[MTuple2[String, String], Long],
+    MTuple2Cheat[MTuple2[String, String], Long]] {
 
-  private var aggregate: mutable.HashMap[(String, String), Long] = null
+  private var aggregate: mutable.HashMap[
+    MTuple2Cheat[MTuple2[String, String], Long],
+    MTuple2Cheat[MTuple2[String, String], Long]] = null
 
   var gcCount = new LongCounter
   var gcTime = new LongCounter
 
   override def open(config: Configuration) {
-    aggregate = mutable.HashMap[(String, String), Long]()
+    aggregate = mutable.HashMap[
+      MTuple2Cheat[MTuple2[String, String], Long],
+      MTuple2Cheat[MTuple2[String, String], Long]]()
     val ctx = getRuntimeContext
     ctx.addAccumulator("gc-time-" + ctx.getIndexOfThisSubtask, gcTime)
     ctx.addAccumulator("gc-count-" + ctx.getIndexOfThisSubtask, gcCount)
   }
 
   override def mapPartition(
-                             records: lang.Iterable[JTuple2[(String, String), Long]],
-                             out: Collector[JTuple2[(String, String), Long]]): Unit = {
+      records: lang.Iterable[MTuple2Cheat[MTuple2[String, String], Long]],
+      out: Collector[MTuple2Cheat[MTuple2[String, String], Long]]): Unit = {
 
     val gcBeans = ManagementFactory.getGarbageCollectorMXBeans.asScala
     var beforeGcCount = 0L
@@ -297,8 +351,12 @@ class HashListAggregator(combine: String)
     val it = records.iterator
     while (it.hasNext) {
       val value = it.next()
-      val agg = aggregate.getOrElse(value._1, 0L)
-      aggregate += (value._1 -> (agg + value._2))
+      val agg = aggregate.getOrElse(
+        value,
+        new MTuple2Cheat(new MTuple2(value._1._1, value._1._2), 0L))
+      agg._2 += value._2
+      aggregate.put(agg, agg)
+//      val newKey = new MTuple2(value._1._1, value._1._2)
     }
 
     var afterGcCount = 0L
@@ -313,106 +371,46 @@ class HashListAggregator(combine: String)
     gcTime.add(afterGcTime - beforeGcTime)
 
 
-    val result = new JTuple2(("", ""), 0L)
-    val outIt = aggregate.iterator
+    val result = new MTuple2Cheat(new MTuple2("", ""), 0L)
+    val outIt = aggregate.values.iterator
 
     while (outIt.hasNext) {
       val value = outIt.next()
-      result._1 = value._1
-      result._2 = value._2
-      out.collect(result)
+      out.collect(value)
     }
   }
 }
 
-
-class HashAggregator(combine: String)
-  extends RichMapPartitionFunction[(String, String, Long), (String, String, Long)] {
-
-  private var aggregate: mutable.HashMap[(String, String), Long] = null
-
-  var gcCount = new LongCounter
-  var gcTime = new LongCounter
-
-  override def open(config: Configuration) {
-    aggregate = mutable.HashMap[(String, String), Long]()
-    val ctx = getRuntimeContext
-    ctx.addAccumulator("gc-time-" + ctx.getIndexOfThisSubtask, gcTime)
-    ctx.addAccumulator("gc-count-" + ctx.getIndexOfThisSubtask, gcCount)
-  }
-
-  override def mapPartition(
-      records: lang.Iterable[(String, String, Long)],
-      out: Collector[(String, String, Long)]): Unit = {
-
-    val gcBeans = ManagementFactory.getGarbageCollectorMXBeans.asScala
-    var beforeGcCount = 0L
-    var beforeGcTime = 0L
-    gcBeans foreach {
-      gc =>
-        beforeGcCount += gc.getCollectionCount
-        beforeGcTime += gc.getCollectionTime
-    }
-
-    val it = records.iterator
-    while (it.hasNext) {
-      val value = it.next()
-      val key = (value._1, value._2)
-      val previous = aggregate.getOrElse(key, 0L)
-      aggregate.put(key, previous + value._3)
-    }
-    val outIt = aggregate.iterator
-
-
-    var afterGcCount = 0L
-    var afterGcTime = 0L
-    gcBeans foreach {
-      gc =>
-        afterGcCount += gc.getCollectionCount
-        afterGcTime += gc.getCollectionTime
-    }
-
-    gcCount.add(afterGcCount - beforeGcCount)
-    gcTime.add(afterGcTime - beforeGcTime)
-
-
-    while (outIt.hasNext) {
-      val value = outIt.next()
-      out.collect((value._1._1, value._1._2, value._2))
-    }
-  }
-}
-
-class HashGrouper(k: Int)
-  extends RichMapPartitionFunction[(String, String, Long), (String, String)] {
-
-  private var groups: mutable.HashMap[String, mutable.MutableList[(String, Long)]] = null
-
-  override def open(config: Configuration) {
-    groups = mutable.HashMap[String, mutable.MutableList[(String, Long)]]()
-  }
-
-  override def mapPartition(
-      records: lang.Iterable[(String, String, Long)],
-      out: Collector[(String, String)]): Unit = {
-    val it = records.iterator()
-    while (it.hasNext) {
-      val value = it.next()
-      val list = groups.getOrElse(value._1, mutable.MutableList[(String, Long)]())
-      list += ((value._2, value._3))
-      groups.put(value._1, list)
-    }
-
-    val listIt = groups.iterator
-    while (listIt.hasNext) {
-      val value = listIt.next()
-      val key = value._1
-      val list = value._2
-      val sorted = list.sortBy(- _._2)
-//      println("SORTED " + sorted.take(5))
-      out.collect((key, sorted.take(k).mkString(", ") ))
-    }
-  }
-}
+//class HashGrouper(k: Int)
+//  extends RichMapPartitionFunction[(String, String, Long), MTuple2[String, String]] {
+//
+//  private var groups: mutable.HashMap[String, mutable.MutableList[(String, Long)]] = null
+//
+//  override def open(config: Configuration) {
+//    groups = mutable.HashMap[String, mutable.MutableList[(String, Long)]]()
+//  }
+//
+//  override def mapPartition(
+//      records: lang.Iterable[(String, String, Long)],
+//      out: Collector[MTuple2[String, String]]): Unit = {
+//    val it = records.iterator()
+//    while (it.hasNext) {
+//      val value = it.next()
+//      val list = groups.getOrElse(value._1, mutable.MutableList[(String, Long)]())
+//      list += ((value._2, value._3))
+//      groups.put(value._1, list)
+//    }
+//
+//    val listIt = groups.iterator
+//    while (listIt.hasNext) {
+//      val value = listIt.next()
+//      val key = value._1
+//      val list = value._2
+//      val sorted = list.sortBy(- _._2)
+////      println("SORTED " + sorted.take(5))
+//      out.collect((key, sorted.take(k).mkString(", ") ))
+//    }
+//  }
+//}
 
 
