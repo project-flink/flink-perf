00484ce87add2d552f61e76e2e109b58c5500abb
Robert Metzger <rmetzger@apache.org>
2014-11-28 11:01:57 +0100

    upd graphx

--- spark-jobs/src/main/scala/com/github/projectflink/spark/pagerank/GraphX.scala
+++ spark-jobs/src/main/scala/com/github/projectflink/spark/pagerank/GraphX.scala
@@ -7,17 +7,11 @@ object GraphX {
   def main(args: Array[String]) {
 
     val master = args(0)
-    val numVertices = args(1).toInt
-    val sparsity = args(2).toDouble
-    val maxIterations = args(3).toInt
-    val output = if(args.length > 4){
-      args(4)
-    }else{
-      null
-    }
-    val input = args(5)
+    val maxIterations = args(1).toInt
+    val input = args(2)
+    val output = args(3)
 
-    val threshold: Double = 0.005 / numVertices
+    print(s"GraphX pageRank master=$master maxIter=$maxIterations input=$input output=$output")
 
     val conf = new SparkConf().setAppName("Spark GraphX PageRank").setMaster(master)
     conf.set("spark.hadoop.skipOutputChecks", "false")
