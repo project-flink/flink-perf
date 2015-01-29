bad3b3f5258a6b44b327be3a4b63e1c67be59c1f
Till Rohrmann <trohrmann@apache.org>
2015-01-29 18:08:38 +0100

    Adds ASF license header and attribution to Spark.

--- flink-jobs/src/main/scala/com/github/projectflink/als/ALSJoinBlocking.scala
+++ flink-jobs/src/main/scala/com/github/projectflink/als/ALSJoinBlocking.scala
@@ -1,5 +1,22 @@
-package com.github.projectflink.als
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.github.projectflink.als
 
 import java.lang
 
@@ -20,6 +37,10 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.util.{Random}
 
+/**
+ * This ALS implementation is based on Spark's MLLib implementation of ALS [https://github.com/
+ * apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/mllib/recommendation/ALS.scala]
+ */
 class ALSJoinBlocking(dop: Int, factors: Int, lambda: Double, iterations: Int, userBlocks: Int,
                       itemBlocks: Int, seed: Long, persistencePath: Option[String]) extends
 ALSFlinkAlgorithm with Serializable {
