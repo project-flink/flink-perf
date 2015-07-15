5372c579ec2f029878614655fe79056e7e11ae7a
Robert Metzger <rmetzger@apache.org>
2015-07-15 13:51:43 +0200

    prepared demo for prime time

--- streaming-state-machine/src/main/scala/com/dataartisans/flink/example/eventpattern/StreamingDemo.scala
+++ streaming-state-machine/src/main/scala/com/dataartisans/flink/example/eventpattern/StreamingDemo.scala
@@ -22,10 +22,14 @@ import _root_.kafka.consumer.ConsumerConfig
 import com.dataartisans.flink.example.eventpattern.kafka.EventDeSerializer
 
 import org.apache.flink.api.common.functions.FlatMapFunction
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.checkpoint.Checkpointed
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.Utils.TypeInformationSerializationSchema
+import org.apache.flink.streaming.connectors.kafka.api.KafkaSink
 import org.apache.flink.streaming.connectors.kafka.api.persistent.PersistentKafkaSource
 import org.apache.flink.util.Collector
+import org.slf4j.{LoggerFactory, Logger}
 
 import scala.collection.mutable
 
@@ -36,27 +40,33 @@ import scala.collection.mutable
  */
 object StreamingDemo {
 
+
   def main(args: Array[String]): Unit = {
-    
+
+    val pt = ParameterTool.fromArgs(args)
     // create the environment to create streams and configure execution
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setParallelism(4)
+    if(pt.has("par")) {
+      env.setParallelism(pt.getInt("par"))
+    }
+
+    if(pt.has("retries")) {
+      env.setNumberOfExecutionRetries(pt.getInt("retries"))
+    }
     
     // this statement enables the checkpointing mechanism with an interval of 1 sec
-    env.enableCheckpointing(1000)
-    
-    // create a data stream from the generator
-    val stream = env.addSource(new EventsGeneratorSource(true))
+    env.enableCheckpointing(pt.getInt("ft", 1000))
+
     
     // data stream from kafka topic.
-//    val props = new Properties()
-//    props.put("zookeeper.connect", "localhost:2181")
-//    props.put("group.id", "flink-demo-group")
-//    props.put("auto.commit.enable", "false")
-//    
-//    val stream = env.addSource(new PersistentKafkaSource[Event]("flink-demo-topic", 
-//                                                                new EventDeSerializer(),
-//                                                                new ConsumerConfig(props)))
+    val props = new Properties()
+    props.put("group.id", "flink-streaming-demo")
+    props.put("auto.commit.enable", "false")
+    props.putAll(pt.toMap)
+
+    val stream = env.addSource(new PersistentKafkaSource[Event](pt.getRequired("topic"),
+                                                                new EventDeSerializer(),
+                                                                new ConsumerConfig(props)))
 
     stream
       // partition on the address to make sure equal addresses
@@ -64,10 +74,10 @@ object StreamingDemo {
       .partitionByHash("sourceAddress")
       
       // the function that evaluates the state machine over the sequence of events
-      .flatMap(new StateMachineMapper())
+      .flatMap(new StateMachineMapper(pt))
       
       // output to standard-out
-      .print()
+      .addSink(new KafkaSink(pt.getRequired("brokerList"), pt.getRequired("error-topic"), props, new TypeInformationSerializationSchema[String]("", env.getConfig)))
     
     // trigger program execution
     env.execute()
@@ -79,21 +89,47 @@ object StreamingDemo {
  * events are consistent with the current state of the state machine. If the event is not
  * consistent with the current state, the function produces an alert.
  */
-class StateMachineMapper extends FlatMapFunction[Event, Alert] with Checkpointed[mutable.HashMap[Int, State]] {
+class StateMachineMapper(val pt: ParameterTool) extends FlatMapFunction[Event, String] with Checkpointed[mutable.HashMap[Int, State]] {
+
+  private val LOG: Logger = LoggerFactory.getLogger(classOf[StateMachineMapper])
   
   private[this] val states = new mutable.HashMap[Int, State]()
+
+  var received = 0L
+  var lastLog = -1L
+  var lastElements = 0L
+  val logfreq = pt.getInt("logFreq")
   
-  override def flatMap(t: Event, out: Collector[Alert]): Unit = {
+  override def flatMap(t: Event, out: Collector[String]): Unit = {
     
     // get and remove the current state
     val state = states.remove(t.sourceAddress).getOrElse(InitialState)
     
     val nextState = state.transition(t.event)
     if (nextState == InvalidTransition) {
-      out.collect(Alert(t.sourceAddress, state, t.event))
+      out.collect(Alert(t.sourceAddress, state, t.event).toString)
     } 
     else if (!nextState.terminal) {
       states.put(t.sourceAddress, nextState)
+    }
+
+    // some statistics
+    received += 1
+    if (received % logfreq == 0) {
+      val now: Long = System.currentTimeMillis
+
+      if (lastLog == -1) {
+        lastLog = now
+        lastElements = received
+      }
+      else {
+        val timeDiff: Long = now - lastLog
+        val elementDiff: Long = received - lastElements
+        val ex: Double = 1000 / timeDiff.toDouble
+        LOG.info(s"During the last $timeDiff ms, we received $elementDiff elements. That's {} elements/second/core", elementDiff * ex)
+        lastLog = now
+        lastElements = received
+      }
     }
   }
 
